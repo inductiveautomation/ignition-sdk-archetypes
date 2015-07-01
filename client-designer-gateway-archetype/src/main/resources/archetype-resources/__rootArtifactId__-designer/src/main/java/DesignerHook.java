b51773f85146bb4e70be3e4c5bf8349c051f81ff
Kevin Herron <kevinherron@gmail.com>
2015-07-01 09:53:15 -0700

    Initial commit

---
+++ client-designer-gateway-archetype/src/main/resources/archetype-resources/__rootArtifactId__-designer/src/main/java/DesignerHook.java
@@ -0,0 +1,23 @@
+package ${package};
+
+import com.inductiveautomation.ignition.common.licensing.LicenseState;
+import com.inductiveautomation.ignition.designer.model.AbstractDesignerModuleHook;
+import com.inductiveautomation.ignition.designer.model.DesignerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DesignerHook extends AbstractDesignerModuleHook {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void startup(DesignerContext context, LicenseState activationState) throws Exception {
+        super.startup(context, activationState);
+    }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+    }
+
+}
---
+++ client-designer-gateway-archetype/src/main/resources/archetype-resources/__rootArtifactId__-gateway/src/main/java/GatewayHook.java
@@ -0,0 +1,28 @@
+package ${package};
+
+import com.inductiveautomation.ignition.common.licensing.LicenseState;
+import com.inductiveautomation.ignition.gateway.model.AbstractGatewayModuleHook;
+import com.inductiveautomation.ignition.gateway.model.GatewayContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GatewayHook extends AbstractGatewayModuleHook {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void setup(GatewayContext gatewayContext) {
+
+    }
+
+    @Override
+    public void startup(LicenseState licenseState) {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+}
